63d296ebd925e3b8aa6d98d13c2b93160802e4b0
Mohammad <moh.osama84x@gmail.com>
2022-11-05 19:48:40 +0200

    initial commit,fetching movies works

---
+++ helpers/tmdb.ts
@@ -0,0 +1,2 @@
+export const url = "https://api.themoviedb.org/3/movie/"
+export const key = process.env.KEY
---
+++ pages/api/movies/popular/index.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import * as tmdb from '../../../../helpers/tmdb'
+import axios from "axios"
+
+export default async function controller(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const response = await axios.get(`${tmdb.url}popular?api_key=${tmdb.key}&language=en-US&page=1`)
+        const data = await response.data
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(400).json(`Error==>${error}`);
+    }
+}
