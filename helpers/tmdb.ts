d454888d4b37dbbca42fa7492855c9abe0d37c8b
Mohammad <moh.osama84x@gmail.com>
2022-11-07 23:14:49 +0200

    home shows movies pictures

--- helpers/tmdb.ts
+++ helpers/tmdb.ts
@@ -1,2 +1,4 @@
 export const url = "https://api.themoviedb.org/3/movie/"
 export const key = process.env.KEY
+export const imgUrl="https://image.tmdb.org/t/p/"
+export const imgSize = 'w500' 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core';
+
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={{
+      breakpoints: {
+        xs: 500,
+        sm: 768,
+        md: 1000,
+        lg: 1200,
+        xl: 1400,
+      },
+    }} withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
