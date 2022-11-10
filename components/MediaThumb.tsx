57da1f7a243e013f8c3b84c22908cdd248f91218
Mohammad <moh.osama84x@gmail.com>
2022-11-10 20:48:43 +0200

    changed card to stack in MediaThumb

--- components/MediaThumb.tsx
+++ components/MediaThumb.tsx
@@ -1,69 +1,51 @@
 import React from 'react'
 import { IMovieOrTv } from '../helpers/types'
-import { Card, Image, Text, Badge, Button, Group, Center } from '@mantine/core';
+import { Card, Image, Text, Badge, Button, Group, Center, Stack } from '@mantine/core';
 import * as tmdb from "./../helpers/tmdb"
 
 interface X {
     media: IMovieOrTv;
 }
-const Moviethumb = ({ media }: X) => {
+const MediaThumb = ({ media }: X) => {
+
     const { id, title, poster_path, vote_average, release_date, genre_ids } = media
+
     return (
-        <Card p="xl" style={{
-            backgroundColor: '#212529',
+        <Stack align="center"
+            justify="space-between"
+            spacing="sm" sx={() => ({
+                backgroundColor:'#212529',
+            })}>
+            <Image
+                src={`${tmdb.imgUrl}${tmdb.imgSize}${poster_path}`}
+                fit="contain"
+                alt={title}
 
-        }}>
-            <Card.Section>
+            />
+            <Text color="white"
+                align="center"
+                size="xl"
+                weight={700}
+                m="md"
+            >
+                {title}
+            </Text>
+            <Group position="center">
+                {genre_ids.map(() => {
 
-                <Image
-                    src={`${tmdb.imgUrl}${tmdb.imgSize}${poster_path}`}
-                    fit="contain"
-                    alt={title}
-
-                />
-
-                <Text color="white"
-                    align="center"
-                    size="xl"
-                    weight={700}
-                    m="md"
-                >
-                    {title}
-                </Text>
-            </Card.Section>
-
-
-            <Card.Section>
-                <Group position="center">
-                    {genre_ids.map(() => {
-
-                        return <Button>
-                            genre
-                        </Button>
-                    })}
-                </Group>
-
-            </Card.Section>
-
-            <Card.Section >
-                <Center style={{ marginBottom: 30, marginTop: 30 }}>
-                    <Text color="white" size="xl">
+                    return <Button>
+                        genre
+                    </Button>
+                })}
+            </Group>
+            <Text color="white" size="xl">
                         {release_date}
                     </Text>
-                </Center>
-
-                <Center style={{ marginBottom: 30, marginTop: 30 }}>
-                    <Badge color="teal" variant="filled" size="lg">
-                        {vote_average}
-                    </Badge>
-                </Center>
-
-            </Card.Section>
-
-        </Card>
-
-
+            <Badge color="teal" variant="filled" size="lg" m="lg">
+                {vote_average}
+            </Badge>
+        </Stack>
     )
 }
 
-export default Moviethumb
+export default MediaThumb
