55d8158dd3310cbad4e5aa99a5c2e5dd806b5c0e
Mohammad <moh.osama84x@gmail.com>
2022-11-20 01:17:47 +0200

    interface ITv

--- helpers/types.ts
+++ helpers/types.ts
@@ -90,6 +90,83 @@ export interface IMovie {
     video: boolean;
     vote_average: number;
     vote_count: number;
+}
 
+export interface ICreatedBy {
+    id: number;
+    credit_id: string;
+    name: string;
+    gender: number;
+    profile_path: string | null;
 
+}
+//check runtime  , show_id
+// add type for crew guest_stars, IPerson maybe ?
+export interface IEpisode {
+    air_date: string;
+    episode_number: number;
+    id: number;
+    name: string;
+    overview: string;
+    production_code: string | null;
+    runtime?: any | null,
+    season_number: number;
+    show_id?: number;
+    still_path: string | null;
+    vote_average: number;
+    vote_count: number;
+    crew?: any;
+    guest_stars?: any;
+
+}
+export interface INetwork {
+    id: number;
+    name: string;
+    logo_path: string | null;
+    origin_country: string;
+}
+
+export interface ISeason {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string;
+    season_number: number;
+}
+// check next expisode to air 
+export interface ITv {
+    adult?: boolean;
+    backdrop_path: string | null;
+    created_by: ICreatedBy[];
+    episode_run_time: [number];
+    first_air_date: string;
+    genres: IGenre[];
+    homepage: string;
+    id: number;
+    in_production: boolean;
+    languages: [string];
+    last_air_date: string;
+    last_episode_to_air: IEpisode;
+    name: string;
+    next_episode_to_air: null,
+    networks: INetwork[];
+    number_of_episodes: number;
+    number_of_seasons: number;
+    origin_country: [string];
+    original_language: string;
+    original_name: string;
+    overview: string;
+    popularity: number;
+    poster_path: string | null;
+    production_companies: IProductionCompany[];
+    production_countries: IProductionCountry[];
+    seasons: ISeason[]
+    spoken_languages: ISpokenLanguage[]
+    status: string;
+    tagline:  string;
+    type:  string;
+    vote_average: number;
+    vote_count: number;
 }
