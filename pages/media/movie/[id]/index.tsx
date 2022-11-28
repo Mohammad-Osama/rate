e144cb7d6853b3b6e77555a45b9ee2c85fbc064b
Mohammad <moh.osama84x@gmail.com>
2022-11-29 01:50:18 +0200

    initial chart

--- pages/media/movie/[id]/index.tsx
+++ pages/media/movie/[id]/index.tsx
@@ -1,53 +1,145 @@
 import React from 'react'
-import { GetStaticProps, GetStaticPropsResult, GetServerSideProps, GetStaticPropsContext, GetServerSidePropsResult, GetServerSidePropsContext } from 'next';
+import { GetServerSidePropsResult, GetServerSidePropsContext } from 'next';
 import * as tmdb from "../../../../helpers/tmdb"
 import { IMovie } from '../../../../helpers/types';
+import { Container, SimpleGrid, Image } from '@mantine/core';
+import {
+    Chart as ChartJS,
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Radar } from 'react-chartjs-2';
 
+ChartJS.register(
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend
+);
+const index = ({ movieInfoProps }: X) => {
+    //  console.log(movieInfoProps)
+      const { id, poster_path, title } = movieInfoProps
 
-const index = ({movieInfoProps}:X) => {
-   console.log(movieInfoProps)
-   const {id} = movieInfoProps
-    return (
-        <div>
-
+    const data = {
+        labels: ['Acting', 'Story', 'Dialogue', 'Directing', 'Cinematography', 'Visual effects','Sound effects'],
+        datasets: [
             {
-           id 
-            }
-        </div>
+                label: 'Average',
+                data: [4, 5, 4, 5, 4, 5, 10],
+                backgroundColor: '#39d353', // label box  background
+                borderColor: '#39d353', // lines 
+                borderWidth: 5,
+                fill: false,
+                fillColor: "red",
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: 'red'
+            },
+        ]
+    };
+    return (
+        <Container size="xl">
+            <SimpleGrid cols={2} spacing="lg"
+                breakpoints={[
+                    { maxWidth: 1024, cols: 2, spacing: 'md' },
+                    { maxWidth: 768, cols: 2, spacing: 'sm' },
+                    { maxWidth: 500, cols: 1, spacing: 'sm' },
+                ]} >
+                 <Image
+                    src={`${tmdb.imgUrl}${tmdb.imgSize}${poster_path}`}
+                    fit="contain"
+                    alt={title}
+                />
+
+                <div style={{}}>
+                    <Radar  options={{
+                        responsive: true,
+                        maintainAspectRatio: true,
+                       
+                        color: "yellow",// color of the main label at the top
+                        scales: {
+                            r: {
+                                min:0,
+                                max: 10,
+                                pointLabels: { // edit labels 
+                                    color: "white" ,
+                                    font:{
+                                       size:15
+                                    }
+                                },
+                                //  reverse,
+                                //  startAngle:33, // rotates the chart
+                                //  suggestedMax:44,
+                                //   suggestedMin,
+                                ticks: {
+                                    display: false,
+                                    stepSize: 1
+                                    
+                                    //  textStrokeColor: 'rgb(54, 162, 235)',
+                                    //   color: 'white',
+                                    //  backdropColor: 'red'
+                                },
+                                angleLines: {
+                                   //     color: 'yellow',
+                                },
+                                //  type,
+                                //  weight,
+                                grid: {
+                                    color: "#2C2E33",
+                                    lineWidth: 2,
+
+                                },
+                            }
+                        }
+                    }}
+                        data={data}
+
+                    //  style={{ minHeight: "100%", minWidth: "100%" }}
+                    />
+                </div>
+                  
+            </SimpleGrid>
+
+
+        </Container>
     )
 }
 
 export default index
 
 interface X {
-    movieInfoProps: IMovie 
-   
+    movieInfoProps: IMovie
+
 }
-export async function getServerSideProps(context:GetServerSidePropsContext): Promise<GetServerSidePropsResult<X>> {
-    const {id,type}=context.query
-     let movieInfo={} as IMovie
-     try {
-        if (type==="movie")
-        {
-            const response = await fetch(`${tmdb.urlMovie}${id}?api_key=${tmdb.key}&language=en-US`)
-            const data = await response.json()
-            movieInfo =data
+export async function getServerSideProps(context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<X>> {
+    const { id, type } = context.query
+    let movieInfo = {} as IMovie
+    try {
+          if (type === "movie") {
+              const response = await fetch(`${tmdb.urlMovie}${id}?api_key=${tmdb.key}&language=en-US`)
+              const data = await response.json()
+              movieInfo = data
+          }
+
+
+
+        return {
+            props: {
+                movieInfoProps: movieInfo
+
+            },
         }
-      
-   
- 
-         return {
-             props: {
-                 movieInfoProps: movieInfo
-                 
-             },
-         }
- 
-     } catch (error) {
-         return {
-             props: {
-                movieInfoProps:{} as IMovie
-             },
-         }
-     }
- } 
+
+    } catch (error) {
+        return {
+            props: {
+                movieInfoProps: {} as IMovie
+            },
+        }
+    }
+}
