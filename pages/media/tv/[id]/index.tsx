9e7fb3cf2845b25f54011f892765e8720f5ccfb5
Mohammad <moh.osama84x@gmail.com>
2023-01-12 04:43:46 +0200

    Carousel for seasons

--- pages/media/tv/[id]/index.tsx
+++ pages/media/tv/[id]/index.tsx
@@ -16,18 +16,9 @@ import {
     Space,
     Card,
     Stack,
-    Divider,
+    Divider
 } from '@mantine/core';
-import {
-    Chart as ChartJS,
-    RadialLinearScale,
-    PointElement,
-    LineElement,
-    Filler,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Radar } from 'react-chartjs-2';
+import { Carousel } from '@mantine/carousel';
 import clientPromise from '../../../../lib/db';
 import AddRate from "../../../../components/AddRate"
 import { useState, useEffect } from 'react';
@@ -323,8 +314,51 @@ const index = ({ tvInfoProps, tvRateInfoProps, tvRateInfoUserProps, tvInfoCredit
                     title={name}
                     media_type={media_type}
                 />
+                <Space h="xl" />
+                <SideTitle text="Seasons"
+                />
+                <Carousel
+                //slideSize="170%"
+                //  height={900}
+                slideGap="sm"
+                controlsOffset="xs"
+                controlSize={40}
+                // dragFree
+                withIndicators
+                slideSize="33.333333%"
+                slidesToScroll={3}
+                styles={{
+                    indicator: {
+                        width: 12,
+                        height: 4,
+                        transition: 'width 250ms ease',
+
+                        '&[data-active]': {
+                            width: 40,
+                        },
+                    },
+                }}
+            >
+                    {seasons.length > 0
+                        ? seasons.map((i) => {
+                            return <Carousel.Slide key={i.id}>
+                                <Image src={`${tmdb.imgUrl}${tmdb.imgSizeW1280}${i.poster_path}`}
+                                    fit="contain"
+                                  //  height="100%"
+                                />
+                            </Carousel.Slide>
+                        })
+                        : <Carousel.Slide >
+                            <Image src={`/images/no_media.jpg`}
+                                fit="contain"
+                                height={400}
+                            />
+                        </Carousel.Slide>
 
 
+                    }
+
+                </Carousel>
                 <Space h={666} />
             </Container>
         )
