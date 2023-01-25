de43f08656fc64bb2b4e94525f5f1a829a2fcad5
Mohammad <moh.osama84x@gmail.com>
2023-01-25 05:33:09 +0200

    IPersonCredits type

--- helpers/types.ts
+++ helpers/types.ts
@@ -3,7 +3,7 @@ export interface IMovieOrTv {
 
     adult?: boolean;
     backdrop_path: string;
-    genre_ids: number[],
+    genre_ids: number[];
     id: number;
     original_language: string;
     original_title?: string;
@@ -385,4 +385,63 @@ export interface IPerson {
     adult: boolean;
     imdb_id: string;
     homepage: string | null;
+}
+
+
+export interface IPersonCreditsCast {
+    adult: boolean;
+    backdrop_path: string | null;
+    genre_ids: number[];
+    id: number;
+    origin_country?: string[];
+    original_language: string;
+    original_name?: string;
+    overview: string;
+    popularity: number;
+    poster_path: string | null;
+    first_air_date?: string;
+    name?: string;
+    vote_average: number;
+    vote_count: number;
+    character: string;
+    credit_id: string;
+    episode_count: number;
+    media_type: string;
+    original_title: string;
+    release_date?: string;
+    title?: string;
+    video?: boolean;
+    order?: number;
+}
+
+export interface IPersonCreditsCrew {
+    adult: boolean;
+    backdrop_path: string | null;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string | null;
+    release_date?: string;
+    title?: string;
+    video?: boolean;
+    vote_average: number;
+    vote_count: number;
+    credit_id: string;
+    department: string;
+    job: string;
+    media_type: string;
+    origin_country?: string[];
+    original_name?: string;
+    first_air_date?: string;
+    name?: string;
+    episode_count?: number;
+}
+
+export interface IPersonCredits {
+    id: number;
+    cast: IPersonCreditsCast[];
+    crew: IPersonCreditsCrew[];
 }
